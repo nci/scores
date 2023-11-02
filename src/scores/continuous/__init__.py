d1d547c3522f1c19f485ffcdfcae6ce5dde03dea
mkhanarm <mohammadreza.khanarmuei@bom.gov.au>
2023-11-02 15:55:43 +1100

    Added isotonic regression
    Fixed directory issue
    
    Fixed pylint related issues
    
    Fixed pylint
    
    Applied review comments

--- src/scores/continuous/__init__.py
+++ src/scores/continuous/__init__.py
@@ -1,5 +1,6 @@
 """
 Import the functions from the implementations into the public API
 """
+from scores.continuous.isoreg import isotonic_fit
 from scores.continuous.murphy_impl import murphy_score, murphy_thetas
 from scores.continuous.standard_impl import mae, mse, rmse
