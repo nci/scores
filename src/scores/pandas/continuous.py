1ff34160c19bb35952350dc8bea4b204268da53f
Harrison Cook <harrison.cook@bom.gov.au>
2024-03-21 14:04:08 +1100

    Add improved docs

--- src/scores/pandas/continuous.py
+++ src/scores/pandas/continuous.py
@@ -1,43 +1,100 @@
 """
 Import the functions from the implementations into the public API
 """
-# pylint: disable=missing-function-docstring
-
-from functools import wraps
 
 from scores import continuous
 from scores.pandas.typing import PandasType
-from scores.typing import FlexibleDimensionTypes
 
 
-@wraps(continuous.mse)
 def mse(
     fcst: PandasType,
     obs: PandasType,
-    reduce_dims: FlexibleDimensionTypes = None,
-    preserve_dims: FlexibleDimensionTypes = None,
     angular: bool = False,
-):
-    return continuous.mse(fcst, obs, reduce_dims=reduce_dims, preserve_dims=preserve_dims, angular=angular)
+) -> PandasType:
+    """Calculates the mean squared error from forecast and observed data.
+
+    A detailed explanation is on [Wikipedia](https://en.wikipedia.org/wiki/Mean_squared_error)
 
 
-@wraps(continuous.rmse)
+    Dimensional reduction is not supported for pandas and the user should
+    convert their data to xarray to formulate the call to the base metric, `scores.continuous.mse`.
+
+    Args:
+        fcst: Forecast or predicted variables in pandas.
+        obs: Observed variables in pandas.
+        angular: specifies whether `fcst` and `obs` are angular
+            data (e.g. wind direction). If True, a different function is used
+            to calculate the difference between `fcst` and `obs`, which
+            accounts for circularity. Angular `fcst` and `obs` data should be in
+            degrees rather than radians.
+
+    Returns:
+        By default a pandas series containing
+        a single floating point number representing the mean squared error for the
+        supplied data. All dimensions will be reduced.
+
+    """
+    return continuous.mse(fcst, obs, angular=angular)
+
+
 def rmse(
     fcst: PandasType,
     obs: PandasType,
-    reduce_dims: FlexibleDimensionTypes = None,
-    preserve_dims: FlexibleDimensionTypes = None,
     angular: bool = False,
-):
-    return continuous.rmse(fcst, obs, reduce_dims=reduce_dims, preserve_dims=preserve_dims, angular=angular)
+) -> PandasType:
+    """Calculate the Root Mean Squared Error from xarray or pandas objects.
+
+    A detailed explanation is on [Wikipedia](https://en.wikipedia.org/wiki/Root-mean-square_deviation)
+
+    Dimensional reduction is not supported for pandas and the user should
+    convert their data to xarray to formulate the call to the base metric, `scores.continuous.rmse`.
+
+    Args:
+        fcst: Forecast or predicted variables in pandas.
+        obs: Observed variables in pandas.
+        angular: specifies whether `fcst` and `obs` are angular
+            data (e.g. wind direction). If True, a different function is used
+            to calculate the difference between `fcst` and `obs`, which
+            accounts for circularity. Angular `fcst` and `obs` data should be in
+            degrees rather than radians.
+
+    Returns:
+        An object containing
+            a single floating point number representing the root mean squared
+            error for the supplied data. All dimensions will be reduced.
+            Otherwise: Returns an object representing the root mean squared error,
+            reduced along the relevant dimensions and weighted appropriately.
+
+    """
+    return continuous.rmse(fcst, obs, angular=angular)
 
 
-@wraps(continuous.mae)
 def mae(
     fcst: PandasType,
     obs: PandasType,
-    reduce_dims: FlexibleDimensionTypes = None,
-    preserve_dims: FlexibleDimensionTypes = None,
     angular: bool = False,
-):
-    return continuous.mae(fcst, obs, reduce_dims=reduce_dims, preserve_dims=preserve_dims, angular=angular)
+) -> PandasType:
+    """Calculates the mean absolute error from forecast and observed data.
+
+    A detailed explanation is on [Wikipedia](https://en.wikipedia.org/wiki/Mean_absolute_error)
+
+
+    Dimensional reduction is not supported for pandas and the user should
+    convert their data to xarray to formulate the call to the base metric, `scores.continuous.mae`.
+
+    Args:
+        fcst: Forecast or predicted variables in pandas.
+        obs: Observed variables in pandas.
+        angular: specifies whether `fcst` and `obs` are angular
+            data (e.g. wind direction). If True, a different function is used
+            to calculate the difference between `fcst` and `obs`, which
+            accounts for circularity. Angular `fcst` and `obs` data should be in
+            degrees rather than radians.
+
+    Returns:
+        By default a pandas series containing
+        a single floating point number representing the mean absolute error for the
+        supplied data. All dimensions will be reduced.
+
+    """
+    return continuous.mae(fcst, obs, angular=angular)
