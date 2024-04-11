14406b3f1fddfaa319d86ea229cf7e9cc62c3e00
Tennessee Leeuwenburg <tennessee.leeuwenburg@bom.gov.au>
2024-04-11 20:16:30 +1000

    Added escape character to mathjax in docstring

--- src/scores/continuous/standard_impl.py
+++ src/scores/continuous/standard_impl.py
@@ -247,7 +247,7 @@ def additive_bias(
 
     It is defined as
     .. math::
-        \\text{Additive bias} =\\frac{1}{N}\sum_{i=1}^{N}(x_i - y_i)
+        \\text{Additive bias} =\\frac{1}{N}\\sum_{i=1}^{N}(x_i - y_i)
         \\text{where } x = \\text{the forecast, and } y = \\text{the observation}
 
 
