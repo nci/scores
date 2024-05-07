1d65cd552bbf8de7d2561ceb8df09fa3a66c1fdb
Tennessee Leeuwenburg <134973832+tennlee@users.noreply.github.com>
2024-05-08 08:55:54 +1000

    Binary Contingency: fix docs and also add missing things to the __init__.py files (#262)
    * Add base classes to the import statement
    * Add contingency classes to the API docs

--- src/scores/categorical/__init__.py
+++ src/scores/categorical/__init__.py
@@ -6,7 +6,9 @@ from scores.categorical.binary_impl import (
     probability_of_false_detection,
 )
 from scores.categorical.contingency_impl import (
+    BasicContingencyManager,
     BinaryContingencyManager,
+    EventOperator,
     ThresholdEventOperator,
 )
 from scores.categorical.multicategorical_impl import firm
@@ -15,6 +17,8 @@ __all__ = [
     "probability_of_detection",
     "probability_of_false_detection",
     "firm",
+    "BasicContingencyManager",
     "BinaryContingencyManager",
     "ThresholdEventOperator",
+    "EventOperator",
 ]
