c94114b5e88dfc87299ef3356e182a61b5c432f3
Tennessee Leeuwenburg <tennessee.leeuwenburg@bom.gov.au>
2024-06-03 17:59:24 +1000

    Update package version to 0.8.4

--- src/scores/__init__.py
+++ src/scores/__init__.py
@@ -13,7 +13,7 @@ import scores.processing
 import scores.sample_data
 import scores.stats.statistical_tests  # noqa: F401
 
-__version__ = "0.8.3"
+__version__ = "0.8.4"
 
 __all__ = [
     "scores.categorical",
