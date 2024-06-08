5f80f16eba786620af7a91e8bb69647cec3505f8
Tennessee Leeuwenburg <tennessee.leeuwenburg@bom.gov.au>
2024-06-08 10:48:13 +1000

    Fix docstring formatting / rendering

--- src/scores/categorical/contingency_impl.py
+++ src/scores/categorical/contingency_impl.py
@@ -889,7 +889,9 @@ class BasicContingencyManager:  # pylint: disable=too-many-public-methods
 
 
         References:
-Ferro, C.A.T. and Stephenson, D.B., 2011. Extremal dependence indices: Improved verification measures for deterministic forecasts of rare binary events. Weather and Forecasting, 26(5), pp.699-713. https://doi.org/10.1175/WAF-D-10-05030.1
+            Ferro, C.A.T. and Stephenson, D.B., 2011. Extremal dependence indices: Improved verification 
+            measures for deterministic forecasts of rare binary events. Weather and Forecasting, 26(5), pp.699-713. 
+            https://doi.org/10.1175/WAF-D-10-05030.1
         """
         score = (
             np.log(self.probability_of_false_detection())
