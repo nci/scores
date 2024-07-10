537fa675d13dda4d47ca1c5a9018fd5383dd4939
Tennessee Leeuwenburg <tennessee.leeuwenburg@bom.gov.au>
2024-07-10 11:32:04 +1000

    Update version information for version 1.0.0

--- src/scores/__init__.py
+++ src/scores/__init__.py
@@ -13,7 +13,7 @@ import scores.processing
 import scores.sample_data
 import scores.stats.statistical_tests  # noqa: F401
 
-__version__ = "0.9.3"
+__version__ = "1.0.0"
 
 __all__ = [
     "scores.categorical",
