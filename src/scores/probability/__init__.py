73d46e88686a40e67ab43767c3af2255f2c85fd7
Nicholas Loveday <48701367+nicholasloveday@users.noreply.github.com>
2024-09-10 17:59:06 -0600

    Adds twcrps for ensembles (#644)
    * added twcrps for ensembles

--- src/scores/probability/__init__.py
+++ src/scores/probability/__init__.py
@@ -10,6 +10,8 @@ from scores.probability.crps_impl import (
     crps_cdf_brier_decomposition,
     crps_for_ensemble,
     crps_step_threshold_weight,
+    tail_tw_crps_for_ensemble,
+    tw_crps_for_ensemble,
 )
 from scores.probability.roc_impl import roc_curve_data
 from scores.processing.isoreg_impl import isotonic_fit
@@ -25,4 +27,6 @@ __all__ = [
     "roc_curve_data",
     "isotonic_fit",
     "crps_step_threshold_weight",
+    "tw_crps_for_ensemble",
+    "tail_tw_crps_for_ensemble",
 ]
