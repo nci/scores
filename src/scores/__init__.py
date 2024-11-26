f936ed951295e3a56b799b84459df3b05be1b952
Tennessee Leeuwenburg <134973832+tennlee@users.noreply.github.com>
2024-11-26 23:13:43 +1100

    Update develop version to 2.0.0 reflecting the breaking API argument change (#781)
    A spelling correction has been made to an argument name, which is technically a breaking change, which necessitates a major version update.

--- src/scores/__init__.py
+++ src/scores/__init__.py
@@ -13,7 +13,7 @@ import scores.processing
 import scores.sample_data
 import scores.stats.statistical_tests  # noqa: F401
 
-__version__ = "1.4.0"
+__version__ = "2.0.0"
 
 __all__ = [
     "scores.categorical",
