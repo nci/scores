ed0b324468f202d74fd60612e8c3c46db4824deb
nicholasloveday <nicholasloveday@gmail.com>
2024-11-27 21:36:22 +1100

    run with updated black

--- src/scores/probability/brier_impl.py
+++ src/scores/probability/brier_impl.py
@@ -25,42 +25,42 @@ def brier_score(
     check_args: bool = True,
 ) -> XarrayLike:
     """
-    Calculates the Brier score for forecast and observed data.
+        Calculates the Brier score for forecast and observed data.
 
-    .. math::
-        \\text{brier score} = \\frac{1}{n} \\sum_{i=1}^n (\\text{forecast}_i - \\text{observed}_i)^2
+        .. math::
+            \\text{brier score} = \\frac{1}{n} \\sum_{i=1}^n (\\text{forecast}_i - \\text{observed}_i)^2
 
-    If you want to speed up performance with Dask and are confident of your input data,
-    or if you want observations to take values between 0 and 1, set ``check_args`` to ``False``.
+        If you want to speed up performance with Dask and are confident of your input data,
+        or if you want observations to take values between 0 and 1, set ``check_args`` to ``False``.
 
-    Args:
-        fcst: Forecast or predicted variables in xarray.
-        obs: Observed variables in xarray.
-        reduce_dims: Optionally specify which dimensions to reduce when
-            calculating the Brier score. All other dimensions will be preserved.
-        preserve_dims: Optionally specify which dimensions to preserve when
-            calculating the Brier score. All other dimensions will be reduced. As a
-            special case, 'all' will allow all dimensions to be preserved. In
-            this case, the result will be in the same shape/dimensionality
-            as the forecast, and the errors will be the absolute error at each
-            point (i.e. single-value comparison against observed), and the
-            forecast and observed dimensions must match precisely.
-        weights: Optionally provide an array for weighted averaging (e.g. by area, by latitude,
-            by population, custom)
-        check_args: will perform some tests on the data if set to True
-    Raises:
-        ValueError: if ``fcst`` contains non-nan values outside of the range [0, 1]
-        ValueError: if ``obs`` contains non-nan values not in the set {0, 1}
+        Args:
+            fcst: Forecast or predicted variables in xarray.
+            obs: Observed variables in xarray.
+            reduce_dims: Optionally specify which dimensions to reduce when
+                calculating the Brier score. All other dimensions will be preserved.
+            preserve_dims: Optionally specify which dimensions to preserve when
+                calculating the Brier score. All other dimensions will be reduced. As a
+                special case, 'all' will allow all dimensions to be preserved. In
+                this case, the result will be in the same shape/dimensionality
+                as the forecast, and the errors will be the absolute error at each
+                point (i.e. single-value comparison against observed), and the
+                forecast and observed dimensions must match precisely.
+            weights: Optionally provide an array for weighted averaging (e.g. by area, by latitude,
+                by population, custom)
+            check_args: will perform some tests on the data if set to True
+        Raises:
+            ValueError: if ``fcst`` contains non-nan values outside of the range [0, 1]
+            ValueError: if ``obs`` contains non-nan values not in the set {0, 1}
 
-<<<<<<< HEAD
-    References:
-        - Brier, G. W. (1950). Verification of forecasts expressed in terms of probability.
-          Monthly Weather Review, 78(1), 1-3. https://doi.org/fp62r6
-        - https://en.wikipedia.org/wiki/Brier_score
-=======
-    See Also:
-        - :py:func:`scores.probability.ensemble_brier_score`
->>>>>>> 64d4dfd (update docs)
+    <<<<<<< HEAD
+        References:
+            - Brier, G. W. (1950). Verification of forecasts expressed in terms of probability.
+              Monthly Weather Review, 78(1), 1-3. https://doi.org/fp62r6
+            - https://en.wikipedia.org/wiki/Brier_score
+    =======
+        See Also:
+            - :py:func:`scores.probability.ensemble_brier_score`
+    >>>>>>> 64d4dfd (update docs)
     """
     if check_args:
         error_msg = ValueError("`fcst` contains values outside of the range [0, 1]")
