aed4880db6f01ad290d80f7d4949454e2f3a61a6
Nikeeth Ramanathan <nikeeth.ramanathan@gmail.com>
2025-02-24 22:17:41 +1100

    [skip ci][wip] revert typing since dim collection is no longer needed

--- src/scores/typing.py
+++ src/scores/typing.py
@@ -3,43 +3,22 @@ This module contains various compound or union types which can be used across th
 a consistent approach to typing is handled.
 """
 
-import functools
-
 from collections.abc import Hashable, Iterable
-from typing import Any, TypeAlias, TypeGuard, Union, cast
+from typing import Union
 
 import pandas as pd
 import xarray
 
-try:
-    # checks that override function signature matches
-    from typing import override
-except ImportError:
-    # does nothing
-    override = lambda _f: _f  # type: ignore - identity composition
+# Flexible Dimension Types should be used for preserve_dims and reduce_dims in all
+# cases across the repository
+FlexibleDimensionTypes = Iterable[Hashable]
 
-# Dimension name. `xarray` recommends `str` for dimension names, however, it doesn't impose
-# restrictions on generic hashables, so we also need to support hashables.
-DimName: TypeAlias = Hashable
+# Xarraylike data types should be used for all forecast, observed and weights
+# However currently some are specified as DataArray only
+XarrayLike = Union[xarray.DataArray, xarray.Dataset]
 
-# `FlexibleDimensionTypes` should be used for `preserve_dims` and `reduce_dims` in all cases across
-# the repository. NOTE: may be replaced by `DimNameCollection` down the track.
-FlexibleDimensionTypes: TypeAlias = Iterable[DimName]
+# These type hint values *may* be used for various arguments across the
+# scores repository but are not establishing a standard or expectation beyond
+# the function they are used in
 
-# `XarrayLike` data types should be used for all forecast, observed and weights. However, currently
-# some are specified as DataArray only
-XarrayLike: TypeAlias = Union[xarray.DataArray, xarray.Dataset]
-
-# `FlexibleArrayType` *may* be used for various arguments across the scores repository, but are not
-# establishing a standard or expectation beyond the function they are used in.
-FlexibleArrayType: TypeAlias = Union[XarrayLike, pd.Series]
-
-# Generic collection that supports both `DimName`, a single hashable and `FlexibleDimensionTypes`.
-# A iterable collection of dimension names. Useful for applying functions that can support both
-# single names or collections e.g. `gather_dimensions` which currently does these checks internally.
-#
-# NOTE: may replace `FlexibleDimensionTypes` down the line - this has more utility functions.
-DimNameCollection: TypeAlias = Union[DimName, Iterable[DimName]]
-
-# --- FIXME: add back typing helpers from test/__TOBEDELETED_nse_refactor_test.py ---
-
+FlexibleArrayType = Union[XarrayLike, pd.Series]
