b77122f594a49bdd59e897e740c1c3ce06226ec0
Stephanie Chong <stephachong@gmail.com>
2025-07-27 18:36:39 +1000

    Add tutorial gallery thumbnail for qunatile interval score and interval score

--- docs/conf.py
+++ docs/conf.py
@@ -58,6 +58,7 @@ nbsphinx_thumbnails = {
     "tutorials/Threshold_Weighted_Scores": "_images/threshold_weighted_scores_thumbnail.png",
     "tutorials/Threshold_Weighted_CRPS_for_Ensembles": "_images/twCRPS_for_ensembles_thumbnail.png",
     "tutorials/Brier_Score": "_images/brier_thumbnail.png",
+    "tutorials/Quantile_Interval_And_Interval_Score": "_images/quantile_interval_score_and_interval_score_thumbnail.png",
 }
 
 # This is needed to allow linking into auto-generated API documentation
