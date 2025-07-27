543c8b6811306df8f5fe59041c5e16965f752a5e
Stephanie Chong <stephachong@gmail.com>
2025-07-27 18:10:28 +1000

    Adds tutorial thumbnail for txCRPS for ensembles

--- docs/conf.py
+++ docs/conf.py
@@ -56,6 +56,7 @@ nbsphinx_thumbnails = {
     "tutorials/NSE": "_images/NSE_thumbnail.png",
     "tutorials/Quantile_Loss": "_images/quantile_loss_thumbnail.png",
     "tutorials/Threshold_Weighted_Scores": "_images/threshold_weighted_scores_thumbnail.png",
+    "tutorials/Threshold_Weighted_CRPS_for_Ensembles": "_images/twCRPS_for_ensembles_thumbnail.png",
 }
 
 # This is needed to allow linking into auto-generated API documentation
