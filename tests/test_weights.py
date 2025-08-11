278c92956a6ffe59735e8d945e2e5ebb4cc2059d
nicholasloveday <nicholasloveday@gmail.com>
2025-08-11 15:51:10 +1000

    add weighted_mean_func

--- tests/test_weights.py
+++ tests/test_weights.py
@@ -4,11 +4,141 @@ Containts tests for weighting scores appropriately.
 
 # pylint: disable=missing-function-docstring
 import numpy as np
+import pytest
 import xarray as xr
 
 import scores.continuous
 import scores.functions
 
+# Tests for apply_weighted_mean
+
+DA_3x3 = xr.DataArray(
+    [[0, 0.5, 1], [2, np.nan, 1], [97, 1, 1]],
+    dims=["x", "y"],
+    coords={"x": [0, 1, 2], "y": [0, 1, 2]},
+)
+
+EXP_DA_X = xr.DataArray(
+    [33, 0.75, 1],
+    dims=["y"],
+    coords={"y": [0, 1, 2]},
+)
+EXP_DA_Y = xr.DataArray(
+    [0.5, 1.5, 33],
+    dims=["x"],
+    coords={"x": [0, 1, 2]},
+)
+EXP_DA_NAN = xr.DataArray(
+    [np.nan, np.nan, np.nan],
+    dims=["x"],
+    coords={"x": [0, 1, 2]},
+)
+EXP_DA_VARY = xr.DataArray(
+    [2 / 3, (2 * 3 + 1) / 4, np.nan],
+    dims=["x"],
+    coords={"x": [0, 1, 2]},
+)
+EXP_DA_NEW_DIM = xr.DataArray(
+    [[33.0, 33.0], [0.75, 0.75], [1.0, 1.0]],
+    dims=["y", "z"],
+    coords={"z": [0, 1], "y": [0, 1, 2]},
+)
+EXP_DA_Y_BROADCAST = xr.DataArray(
+    [1 / 3, 2, 33],
+    dims=["x"],
+    coords={"x": [0, 1, 2]},
+)
+EXP_DA_VARY_XY = xr.DataArray(1.1)
+WEIGHTS1 = xr.DataArray(
+    [[2, 2, 2], [2, 2, 2], [2, 2, 2]],
+    dims=["x", "y"],
+    coords={"x": [0, 1, 2], "y": [0, 1, 2]},
+)
+WEIGHTS2 = WEIGHTS1 * 0
+WEIGHTS3 = WEIGHTS1 * np.nan
+WEIGHTS4 = xr.DataArray(
+    [[1, 2, 3], [3, 2, 1], [0, np.nan, np.nan]],
+    dims=["x", "y"],
+    coords={"x": [0, 1, 2], "y": [0, 1, 2]},
+)
+WEIGHTS5 = xr.DataArray(
+    [1, 2],
+    dims=["z"],
+    coords={"z": [0, 1]},
+)
+WEIGHTS6 = xr.DataArray(
+    [1, 2, np.nan],
+    dims=["y"],
+    coords={"y": [0, 1, 3]},
+)
+
+
+@pytest.mark.parametrize(
+    ("values", "reduce_dims", "weights", "expected"),
+    [
+        # No reduction
+        (DA_3x3, None, None, DA_3x3),
+        # Unweighted mean over x
+        (DA_3x3, ["x"], None, EXP_DA_X),
+        # Unweighted mean over y
+        (DA_3x3, ["y"], None, EXP_DA_Y),
+        # Equal weights over y
+        (DA_3x3, ["y"], WEIGHTS1, EXP_DA_Y),
+        # Zero weights over y
+        (DA_3x3, ["y"], WEIGHTS2, EXP_DA_NAN),
+        # NaN weights over y
+        (DA_3x3, ["y"], WEIGHTS3, EXP_DA_NAN),
+        # Varying weights over y
+        (DA_3x3, ["y"], WEIGHTS4, EXP_DA_VARY),
+        # Extra dim in weights
+        (DA_3x3, ["x"], WEIGHTS5, EXP_DA_NEW_DIM),
+        # Weights only for 1 dim to test broadcasting, with NaN
+        (DA_3x3, ["y"], WEIGHTS6, EXP_DA_Y_BROADCAST),
+        # Varying weights over x and y
+        (DA_3x3, ["x", "y"], WEIGHTS4, EXP_DA_VARY_XY),
+        # Varying weights over y, but input error is NaN
+        (DA_3x3 * np.nan, ["y"], WEIGHTS4, EXP_DA_VARY * np.nan),
+    ],
+)
+def test_apply_weighted_mean(values, reduce_dims, weights, expected):
+    """
+    Tests scores.functions.apply_weighted_mean
+    #"""
+    # Check with xr.DataArray
+    result = scores.functions.apply_weighted_mean(values, reduce_dims=reduce_dims, weights=weights)
+    xr.testing.assert_equal(result, expected)
+
+    # Check with xr.Dataset
+    values_ds = xr.Dataset(({"var1": values, "var2": values}))
+    result_ds = scores.functions.apply_weighted_mean(values_ds, reduce_dims=reduce_dims, weights=weights)
+    expected_ds = xr.Dataset(({"var1": expected, "var2": expected}))
+    xr.testing.assert_equal(result_ds, expected_ds)
+
+
+def test_apply_weighted_mean_warns():
+    """
+    Test that a warning is raised if weights are provided but reduce_dims is None.
+    """
+    with pytest.warns(UserWarning) as record:
+        result = scores.functions.apply_weighted_mean(DA_3x3, reduce_dims=None, weights=WEIGHTS1)
+    xr.testing.assert_equal(DA_3x3, result)
+
+
+def test_apply_weighted_mean_raises():
+    """
+    Test that a Value error is raised if there are negative weights
+    """
+    negative_weights = xr.DataArray(
+        [[1, -1, 1], [1, 1, 1], [1, 1, 1]],
+        dims=["x", "y"],
+        coords={"x": [0, 1, 2], "y": [0, 1, 2]},
+    )
+
+    with pytest.raises(ValueError, match="Weights must not contain negative values."):
+        scores.functions.apply_weighted_mean(DA_3x3, reduce_dims=["x"], weights=negative_weights)
+
+
+# Old tests
 ZERO = np.array([[1 for i in range(10)] for j in range(10)])
 
 
