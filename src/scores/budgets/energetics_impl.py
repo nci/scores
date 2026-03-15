e3441b359680e6e3a3317e39ac41acbebc53db7c
davelee2804 <davelee2804@gmail.com>
2026-03-16 10:48:51 +1100

    updating the interfaces and adding docstrings

--- src/scores/budgets/energetics_impl.py
+++ src/scores/budgets/energetics_impl.py
@@ -5,70 +5,86 @@ from scores.budgets.budgets_utils import *
 from scores.typing import XarrayLike
 
 def prepare_fields(
-        fourDFields: XarrayLike,
-        threeDFields: XarrayLike,
-        twoDFields: XarrayLike,
+        fields: XarrayLike,
         sub_domain_longitude = np.array([None]),
-        sub_domain_latitude = np.array([None]),
-        sub_domain_time = [],
-        sub_domain_level = np.array([None])):
+        sub_domain_latitude = np.array([None])):
+    '''
+    Select the subdomain in longitude and latitude over which the energetic integrals
+    are to be computed.
 
-    twoDFields.sortby("latitude")
-    threeDFields.sortby("latitude")
-    fourDFields.sortby("latitude")
+    Args:
+        fields: Input fields for the 4D space-time quantities, 3D space-time quantities and 2D time only 
+            quantities used to compute the energetics, and their space time dimensional attributes
+        sub_domain_longitude: array containing the minimum and maximum values of the sub-domain over which the
+            energy components are to be computed in the longitudinal direction (optional)
+        sub_domain_latitude: array containing the minimum and maximum values of the sub-domain over which the
+            energy components are to be computed in the latitudinal direction (optional)
 
-    if sub_domain_time != []:
-        threeDFields = threeDFields.sel(time=sub_domain_time)
-        fourDFields = fourDFields.sel(time=sub_domain_time)
+    Returns:
+        The fields within the sub-domain region.
+    '''
 
-    if sub_domain_level.any() != None:
-        fourDFields = fourDFields.sel(level=sub_domain_level)
+    fields.sortby("latitude")
 
     if sub_domain_longitude.any() != None:
-        twoDFields = twoDFields.sel(longitude=sub_domain_longitude)
-        threeDFields = threeDFields.sel(longitude=sub_domain_longitude)
-        fourDFields = fourDFields.sel(longitude=sub_domain_longitude)
+        fields = fields.sel(longitude=sub_domain_longitude)
 
     if sub_domain_latitude.any() != None:
-        twoDFields = twoDFields.sel(latitude=sub_domain_latitude)
-        threeDFields = threeDFields.sel(latitude=sub_domain_latitude)
-        fourDFields = fourDFields.sel(latitude=sub_domain_latitude)
+        fields = fields.sel(latitude=sub_domain_latitude)
 
-    return fourDFields, threeDFields, twoDFields
+    return fields
 
 def energy_components(
-        fourDFields: XarrayLike,
-        threeDFields: XarrayLike,
-        twoDFields: XarrayLike,
+        fields: XarrayLike,
+        fieldnames,
         sub_domain_longitude = np.array([None]),
         sub_domain_latitude = np.array([None]),
-        sub_domain_time = [],
-        sub_domain_level = np.array([None]),
         output_file_name = [],
         ) -> XarrayLike:
-
     '''
-    compute the time series for the energy budget on pressure levels
-    for details see:
+    Compute the time series for the energy budget on pressure levels
+
+    .. math::
+        \\text{Internal}  = \\frac{1}{g}\\int_{p_1}^{p_0}\\int_{\\Omega}(c_p(1-q) + c_{pv}q)T\\text{d}\\Omega\\text{d}p
+        \\text{Latent}    = \\frac{1}{g}\\int_{p_1}^{p_0}\\int_{\\Omega}L_v q\\text{d}\\Omega\\text{d}p
+        \\text{Potential} = \\frac{1}{g}\\int_{\\Omega}\\Phi_s\\text{d}\\Omega
+        \\text{Kinetic}   = \\frac{1}{g}\\int_{p_1}^{p_0}\\int_{\\Omega}\\frac{1}{2}(u^2 + v^2 + w^2)\\text{d}\\Omega\\text{d}p
+
+    Args:
+        fields: Input fields for the 4D space-time quantities used to compute the energy components (specifically 
+            the water vapor, temperature and the zonal, meridional and vertical velocities, and the 3D space-time
+            surface pressure, and the 2D space only surface geopotential), and their space time dimensional 
+            attributes
+        fieldnames: list of strings denoting the names for the different fields, specifically - 0: zonal velocity, 
+            1: meridional velocity, 2: vertical velocity, 3: temperature, 4: water vapor, 6: surface pressure,
+            7: surface geopotential
+        sub_domain_longitude: array containing the minimum and maximum values of the sub-domain over which the
+            energy components are to be computed in the longitudinal direction (optional)
+        sub_domain_latitude: array containing the minimum and maximum values of the sub-domain over which the
+            energy components are to be computed in the latitudinal direction (optional)
+        output_file_name: name of the test file to which to write the output (optional)
+            
+    Returns:
+        2D array containing the time series for the domain integrals of the energy components at each time
+
+    References:
         Trenberth et. al. J. Clim. (2002) v. 15 pp 3343--3360, eqn (5)
         Sha et. al (2025), eqn (12)
-
-        1/g\int_{p1}^{p0} c_p* T + L_v q + \Phi_s + 0.5*(u*u + v*v + w*w) dp
-        c_p* = cp(1-q) + c_{pv}q
+        Taylor, M. "Conservation of Mass and Energy for the Moist 
+        Atmospheric Primitive Equations on Unstructured Grids", (2011) Section 12.4.5
     '''
-    dlon, dlat, lon, lat = integration_weights(fourDFields.longitude.values, fourDFields.latitude.values, \
+    dlon, dlat, lon, lat = integration_weights(fields.longitude.values, fields.latitude.values, \
             sub_domain_longitude, sub_domain_latitude)
 
-    # select the temporal, vertical and horizontal sub-domain
-    fourDFields, threeDFields, twoDFields = prepare_fields(fourDFields, threeDFields, twoDFields, \
-            lon, lat, sub_domain_time, sub_domain_level)
+    # select the latitude and longitude sub-domain
+    fields = prepare_fields(fields, lon, lat)
 
     # get the surface geopotential from the 2d fields
-    zs = twoDFields["z_surf"]
+    zs = fields[fieldnames[7]]
 
     dp = pressure_level_thickness(sub_domain_level)
 
-    nt = len(fourDFields.time)
+    nt = len(fields.time)
 
     I = np.zeros(nt)    # global integral of the interal energy
     L = np.zeros(nt)    # global integral of the latent energy
@@ -76,30 +92,23 @@ def energy_components(
     Kh = np.zeros(nt)   # global integral of the kinetic energy (horizontal component)
     Kv = np.zeros(nt)   # global integral of the kinetic energy (vertical component)
 
-    if sub_domain_time != []:
-        time_array = sub_domain_time
-    else:
-        time_array = fourDFields.time.values
-
-    if sub_domain_level.any() != None:
-        level_array = sub_domain_level
-    else:
-        level_array = fourDFields.level.values
+    time_array = fields.time.values
+    level_array = fields.level.values
 
     tt = 0
     for _time in time_array:
         #  From Taylor (2011), eqn (12.8)
-        sp    = threeDFields["sp"].sel(time=_time)
+        sp    = fields[fieldnames[6]].sel(time=_time)
         sp_zs = sp*zs
         P[tt] = integrate_horizontal(sp_zs,dlon,dlat)
 
         ll = 0
         for _level in level_array:
-            ult = fourDFields["u"].sel(level=_level,time=_time)
-            vlt = fourDFields["v"].sel(level=_level,time=_time)
-            wlt = fourDFields["w"].sel(level=_level,time=_time)
-            tlt = fourDFields["t"].sel(level=_level,time=_time)
-            qlt = fourDFields["q"].sel(level=_level,time=_time)
+            ult = fields[fieldnames[0]].sel(level=_level,time=_time)
+            vlt = fields[fieldnames[1]].sel(level=_level,time=_time)
+            wlt = fields[fieldnames[2]].sel(level=_level,time=_time)
+            tlt = fields[fieldnames[3]].sel(level=_level,time=_time)
+            qlt = fields[fieldnames[4]].sel(level=_level,time=_time)
 
             khlt = ult*ult + vlt*vlt
             kvlt = wlt*wlt
@@ -129,26 +138,51 @@ def energy_components(
     return energy_integrals
 
 def energy_exchanges(
-        fourDFields: XarrayLike,
-        threeDFields: XarrayLike,
-        twoDFields: XarrayLike,
+        fields: XarrayLike,
+        fieldnames,
         sub_domain_longitude = np.array([None]),
         sub_domain_latitude = np.array([None]),
-        sub_domain_time = [],
-        sub_domain_level = np.array([None]),
         output_file_name = [],
         ) -> XarrayLike:
+    """
+    Compute the exchanges between kinetic to internal, internal to kinetic, kinetic to potential and
+    potential to kinetic energies.
 
-    dlon, dlat, lon, lat = integration_weights(fourDFields.longitude.values, fourDFields.latitude.values, \
+    .. math::
+        \\text{Kinetic to Internal}  = \\int_{p_1}^{p_0}\\int_{\\Omega}\\nabla (z-z_s)\\cdot\\boldsymbol{u}\\text{d}\\Omega\\text{d}p
+        \\text{Internal to Kinetic}  = \\int_{p_1}^{p_0}\\int_{\\Omega}(z-z_s) \\nabla\\cdot\\boldsymbol{u}\\text{d}\\Omega\\text{d}p
+        \\text{Kinetic to Potential} = \\int_{p_1}^{p_0}\\int_{\\Omega}\\nabla z_s\\cdot\\boldsymbol{u}\\text{d}\\Omega\\text{d}p
+        \\text{Potential to Kinetic} = \\int_{p_1}^{p_0}\\int_{\\Omega}z_s \\nabla\\cdot\\boldsymbol{u}\\text{d}\\Omega\\text{d}p
+
+    Args:
+        fields: Input fields for the 4D space-time quantities used to compute the energy components (specifically 
+            the zonal and meridional velocities, and the geopotential, and the 2D space only surface geopotential), 
+            and their space time dimensional attributes
+        fieldnames: list of strings denoting the names for the different fields, specifically - 0: zonal velocity, 
+            1: meridional velocity, 2: vertical velocity, 5: geopotential, 7: surface geopotential
+        sub_domain_longitude: array containing the minimum and maximum values of the sub-domain over which the
+            energy components are to be computed in the longitudinal direction (optional)
+        sub_domain_latitude: array containing the minimum and maximum values of the sub-domain over which the
+            energy components are to be computed in the latitudinal direction (optional)
+        output_file_name: name of the test file to which to write the output (optional)
+
+    Returns:
+        2D array containing the time series for the domain integrals of the energy exchanges at each time
+
+    References:
+        Taylor, M. "Conservation of Mass and Energy for the Moist 
+        Atmospheric Primitive Equations on Unstructured Grids", (2011) Section 12.4.5
+    """
+
+    dlon, dlat, lon, lat = integration_weights(fields.longitude.values, fields.latitude.values, \
             sub_domain_longitude, sub_domain_latitude)
 
     cos_theta, sin_theta, cos_theta_inv = trig_fields(lon, lat)
 
     # select the temporal, vertical and horizontal sub-domain
-    fourDFields, threeDFields, twoDFields = prepare_fields(fourDFields, threeDFields, twoDFields, \
-            lon, lat, sub_domain_time, sub_domain_level)
+    fields = prepare_fields(fields, lon, lat)
 
-    nt = len(fourDFields.time)
+    nt = len(fields.time)
 
     KtoI = np.zeros(nt) # kinetic to internal energy exchange (horizontal)
     ItoK = np.zeros(nt) # internal to kinetic energy exchange (horizontal)
@@ -157,25 +191,18 @@ def energy_exchanges(
 
     dp = pressure_level_thickness(sub_domain_level)
 
-    if sub_domain_time != []:
-        time_array = sub_domain_time
-    else:
-        time_array = fourDFields.time.values
+    time_array = fields.time.values
+    level_array = fields.level.values
 
-    if sub_domain_level.any() != None:
-        level_array = sub_domain_level
-    else:
-        level_array = fourDFields.level.values
-
-    zs = twoDFields["z_surf"]
+    zs = fields[fieldnames[7]]
 
     tt = 0
     for _time in time_array:
         ll = 0
         for _level in level_array:
-            ult = fourDFields["u"].sel(level=_level,time=_time)
-            vlt = fourDFields["v"].sel(level=_level,time=_time)
-            zlt = fourDFields["z"].sel(level=_level,time=_time)
+            ult = fields[fieldnames[0]].sel(level=_level,time=_time)
+            vlt = fields[fieldnames[1]].sel(level=_level,time=_time)
+            zlt = fields[fieldnames[5]].sel(level=_level,time=_time)
 
             z_m_zs = zlt - zs
 
@@ -195,7 +222,7 @@ def energy_exchanges(
             KtoP_str = "{:16.15e}".format(KtoP[tt])
             PtoK_str = "{:16.15e}".format(PtoK[tt])
 
-            with open(outfilename, "a") as outfile:
+            with open(output_file_name, "a") as outfile:
                 outfile.write(f"{KtoI_str}\t{ItoK_str}\t{KtoP_str}\t{PtoK_str}\n")
 
         tt = tt + 1
