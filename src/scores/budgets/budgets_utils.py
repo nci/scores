mograted all numpy operations to xarray equivalents (dimension indiex agnostic), and also fixed a conversion bug in the gradients for the energy exchanges, and updated the tests accordingly

src/scores/budgets/budgets_utils.py
@@ -82,23 +82,33 @@ def integration_weights(longitude, latitude, sub_domain_lon=np.array([None]), su
 
     dlat[:] = METERS_PER_DEGREE * dlat[:]
 
-    return dlon, dlat, longitude, latitude
+    return xr.DataArray(dlon, dims="longitude", coords={"longitude": longitude}), xr.DataArray(
+        dlat, dims="latitude", coords={"latitude": latitude}
+    )
 
 
 # dimensions are assumed as: field[num_latitudes,num_longitudes]
 def integrate_horizontal(field, dlon, dlat):
-    int_lon = np.dot(field, dlon)
-    int_tot = np.dot(int_lon, dlat)
+    int_lon = field.dot(dlon)
+    int_tot = int_lon.dot(dlat)
     return int_tot
 
 
 def trig_fields(longitude, latitude):
     nlon = len(longitude)
 
-    lat_rad = np.deg2rad(latitude)
+    lat_rad = np.deg2rad(latitude).to_numpy()
 
-    cos_theta = np.cos(lat_rad)[:, None] * np.ones((1, nlon))
-    sin_theta = np.sin(lat_rad)[:, None] * np.ones((1, nlon))
+    cos_theta_np = np.cos(lat_rad)[:, None] * np.ones((1, nlon))
+    sin_theta_np = np.sin(lat_rad)[:, None] * np.ones((1, nlon))
+
+    cos_theta = xr.DataArray(
+        cos_theta_np, dims=["latitude", "longitude"], coords={"latitude": latitude, "longitude": longitude}
+    )
+
+    sin_theta = xr.DataArray(
+        sin_theta_np, dims=["latitude", "longitude"], coords={"latitude": latitude, "longitude": longitude}
+    )
 
     cos_theta_inv = 1.0 / cos_theta
 
@@ -123,7 +133,7 @@ def pressure_level_thickness(levels):
 
 
 def integrate_energy_exchange(
-    field_scalar, field_vector_x, field_vector_y, longitude, latitude, dlon, dlat, cos_theta, sin_theta, cos_theta_inv
+    field_scalar, field_vector_x, field_vector_y, dlon, dlat, cos_theta, sin_theta, cos_theta_inv
 ):
     r"""
     Williamson et. al., JCP (1992), eqns (3-4):
@@ -134,18 +144,18 @@ def integrate_energy_exchange(
     div(u):  1/(r \cos(theta)) (d u/d lambda + d(v\cos(theta))/d theta)
     """
 
-    _dlon = np.abs(np.deg2rad(longitude[1] - longitude[0]) * METERS_PER_DEGREE)
-    _dlat = np.abs(np.deg2rad(latitude[1] - latitude[0]) * METERS_PER_DEGREE)
-
     # grad f:  1/(r \cos(\theta)) df/d\lambda, 1/r df/d\theta
-    dfdx = np.gradient(field_scalar, _dlon, axis=3) * cos_theta_inv
-    dfdy = np.gradient(field_scalar, _dlat, axis=2)
+    dfdx = field_scalar.differentiate("longitude") * cos_theta_inv / METERS_PER_DEGREE
+    dfdy = field_scalar.differentiate("latitude") / METERS_PER_DEGREE
     grad_f_dot_u = dfdx * field_vector_x + dfdy * field_vector_y
     int_grad_f_dot_u = integrate_horizontal(grad_f_dot_u, dlon, dlat)
 
     # div(u):  1/(r \cos(\theta)) (du/d\lambda + d(v\cos(\theta))/d\theta)
-    dudx = np.gradient(field_vector_x, _dlon, axis=3)
-    dvdy = np.gradient(field_vector_y, _dlat, axis=2) * cos_theta - sin_theta * field_vector_y / METERS_PER_DEGREE
+    dudx = field_vector_x.differentiate("longitude") / METERS_PER_DEGREE
+    dvdy = (
+        field_vector_y.differentiate("latitude") * cos_theta / METERS_PER_DEGREE
+        - sin_theta * field_vector_y / RAD_EARTH
+    )
     div_u = cos_theta_inv * (dudx + dvdy)
     f_div_u = field_scalar * div_u
     int_f_div_u = integrate_horizontal(f_div_u, dlon, dlat)

src/scores/budgets/energetics_impl.py
@@ -80,8 +80,6 @@ def energy_components(
         2D array containing the time series for the domain integrals of the energy components at each time
 
     References:
-        Taylor, M. "Conservation of Mass and Energy for the Moist
-        Atmospheric Primitive Equations on Unstructured Grids", (2011) Section 12.4.5
         Trenberth, K. E., Stepaniak, D. P., Caron, J. M. (2002) "Accuracy of Atmospheric Energy Budgets from Analyses"
           J. Clim. 15 3343--3360
         Sha, Y., Schreck, J. S., Chapman, W., Gagne, D. J. (2025) "Improving AI Weather Prediction Models using Global
@@ -94,12 +92,12 @@ def energy_components(
     if fields.longitude.values[0] < -0.1:
         fields = resort_lon_from_m180to180_to_0to360(fields, "longitude")
 
-    dlon, dlat, lon, lat = integration_weights(
+    dlon, dlat = integration_weights(
         fields.longitude.values, fields.latitude.values, sub_domain_longitude, sub_domain_latitude
     )
 
     # select the latitude and longitude sub-domain
-    fields = prepare_fields(fields, lon, lat, sub_domain_longitude, sub_domain_latitude)
+    fields = prepare_fields(fields, dlon.longitude, dlat.latitude, sub_domain_longitude, sub_domain_latitude)
 
     nt = len(fields.time)
 
@@ -179,21 +177,22 @@ def energy_exchanges(
         2D array containing the time series for the domain integrals of the energy exchanges at each time
 
     References:
-        Taylor, M. "Conservation of Mass and Energy for the Moist
-        Atmospheric Primitive Equations on Unstructured Grids", (2011) Section 12.4.5
+        Taylor, M. A. (2011). Conservation of mass and energy for the moist atmospheric primitive equations on
+          unstructured grids. In P. H. Lauritzen, et al. (Eds.), Numerical techniques for global atmospheric models,
+          Lecture Notes Comput. Sci. Eng. (Vol. 80, pp. 357–380). Heidelberg, Germany: Springer.
     """
     # re-order the longitudes to range between 0 and 360 degrees (global)
     if fields.longitude.values[0] < -0.1:
         fields = resort_lon_from_m180to180_to_0to360(fields, "longitude")
 
-    dlon, dlat, lon, lat = integration_weights(
+    dlon, dlat = integration_weights(
         fields.longitude.values, fields.latitude.values, sub_domain_longitude, sub_domain_latitude
     )
 
-    cos_theta, sin_theta, cos_theta_inv = trig_fields(lon, lat)
+    cos_theta, sin_theta, cos_theta_inv = trig_fields(fields.longitude, fields.latitude)
 
     # select the temporal, vertical and horizontal sub-domain
-    fields = prepare_fields(fields, lon, lat, sub_domain_longitude, sub_domain_latitude)
+    fields = prepare_fields(fields, dlon.longitude, dlat.latitude, sub_domain_longitude, sub_domain_latitude)
 
     nt = len(fields.time)
 
@@ -217,17 +216,13 @@ def energy_exchanges(
     dp_x = xr.DataArray(np.zeros((nt, len(dp))), dims=("time", "level"))
     dp_x = dp_x + dp
 
-    zs_v = xr.DataArray(np.zeros((nt, len(dp), len(lat), len(lon))), dims=("time", "level", "latitude", "longitude"))
+    zs_v = xr.DataArray(np.zeros((nt, len(dp), len(dlat), len(dlon))), dims=("time", "level", "latitude", "longitude"))
     zs_v = zs_v + zs
 
     z_m_zs = zlt - zs_v
 
-    KtoI_t, ItoK_t = integrate_energy_exchange(
-        z_m_zs, ult, vlt, lon, lat, dlon, dlat, cos_theta, sin_theta, cos_theta_inv
-    )
-    KtoP_t, PtoK_t = integrate_energy_exchange(
-        zs_v, ult, vlt, lon, lat, dlon, dlat, cos_theta, sin_theta, cos_theta_inv
-    )
+    KtoI_t, ItoK_t = integrate_energy_exchange(z_m_zs, ult, vlt, dlon, dlat, cos_theta, sin_theta, cos_theta_inv)
+    KtoP_t, PtoK_t = integrate_energy_exchange(zs_v, ult, vlt, dlon, dlat, cos_theta, sin_theta, cos_theta_inv)
     KtoP = (dp_x * KtoP_t).sum(dim="level")
     PtoK = (dp_x * PtoK_t).sum(dim="level")
     KtoI = (dp_x * KtoI_t).sum(dim="level")

tests/budgets/test_energetics.py
@@ -163,25 +163,20 @@ def test_budgets_integral(
         longitude = np.linspace(-180.0, +180.0, nlon, endpoint=False)
         latitude = np.linspace(-90.0, +90.0, nlat, endpoint=False)
 
-        dlon, dlat, lon, lat = integration_weights(longitude, latitude, sub_domain_longitude, sub_domain_latitude)
-        nlon = len(lon)
-        nlat = len(lat)
+        dlon, dlat = integration_weights(longitude, latitude, sub_domain_longitude, sub_domain_latitude)
+        nlon = len(dlon)
+        nlat = len(dlat)
 
-        psi = np.zeros((nlat, nlon))
-        for ii in np.arange(nlat):
-            for jj in np.arange(nlon):
-                psi[ii, jj] = field(lon[jj], lat[ii], alpha) + offset
+        lon2d, lat2d = np.meshgrid(dlon.longitude, dlat.latitude)
+        _psi = field(lon2d, lat2d, alpha) + offset
+
+        psi = xr.DataArray(
+            _psi, dims=["latitude", "longitude"], coords={"latitude": dlat.latitude, "longitude": dlon.longitude}
+        )
 
         int_psi = integrate_horizontal(psi, dlon, dlat)
 
-        error_at_res[res] = (analytic_solution - int_psi) / analytic_solution
-
-        print(
-            str(res)
-            + ":\t{:.4e}".format(analytic_solution)
-            + "\t{:.4e}".format(int_psi)
-            + "\t{:.4e}".format(error_at_res[res])
-        )
+        error_at_res[res] = (analytic_solution - int_psi.data) / analytic_solution
 
     convergence = np.zeros(num_resolutions - 1)
     for res in np.arange(num_resolutions - 1):
@@ -250,39 +245,40 @@ def test_budgets_gradient(
         longitude = np.linspace(-180.0, +180.0, nlon)
         latitude = np.linspace(-90.0, +90.0, nlat)
 
-        dlon, dlat, lon, lat = integration_weights(longitude, latitude, sub_domain_longitude, sub_domain_latitude)
-        nlon = len(lon)
-        nlat = len(lat)
-        cos_theta, sin_theta, cos_theta_inv = trig_fields(lon, lat)
+        dlon, dlat = integration_weights(longitude, latitude, sub_domain_longitude, sub_domain_latitude)
+        nlon = len(dlon)
+        nlat = len(dlat)
+        cos_theta, sin_theta, cos_theta_inv = trig_fields(dlon.longitude, dlat.latitude)
 
-        psi = np.zeros((1, 1, nlat, nlon))
-        dPsiDx = np.zeros((1, 1, nlat, nlon))
-        dPsiDy = np.zeros((1, 1, nlat, nlon))
-        lap_psi = np.zeros((1, 1, nlat, nlon))
-        for ii in np.arange(nlat):
-            for jj in np.arange(nlon):
-                psi[0, 0, ii, jj] = field(lon[jj], lat[ii], alpha)
-                dPsiDx[0, 0, ii, jj] = zonal_gradient(lon[jj], lat[ii], alpha)
-                dPsiDy[0, 0, ii, jj] = meridional_gradient(lon[jj], lat[ii], alpha)
-                lap_psi[0, 0, ii, jj] = div_vec(lon[jj], lat[ii], alpha)
+        lon2d, lat2d = np.meshgrid(dlon.longitude, dlat.latitude)
+        _psi = field(lon2d, lat2d, alpha)
+        _dPsiDx = zonal_gradient(lon2d, lat2d, alpha)
+        _dPsiDy = meridional_gradient(lon2d, lat2d, alpha)
+        _lapPsi = div_vec(lon2d, lat2d, alpha)
 
-        grad_f_dot_u, f_div_u = integrate_energy_exchange(
-            psi, dPsiDx, dPsiDy, lon, lat, dlon, dlat, cos_theta, sin_theta, cos_theta_inv
+        psi = xr.DataArray(
+            _psi, dims=["latitude", "longitude"], coords={"latitude": dlat.latitude, "longitude": dlon.longitude}
+        )
+        dPsiDx = xr.DataArray(
+            _dPsiDx, dims=["latitude", "longitude"], coords={"latitude": dlat.latitude, "longitude": dlon.longitude}
+        )
+        dPsiDy = xr.DataArray(
+            _dPsiDy, dims=["latitude", "longitude"], coords={"latitude": dlat.latitude, "longitude": dlon.longitude}
+        )
+        lapPsi = xr.DataArray(
+            _lapPsi, dims=["latitude", "longitude"], coords={"latitude": dlat.latitude, "longitude": dlon.longitude}
         )
 
-        err1 = grad_f_dot_u[:, :] - (dPsiDx[0, 0, :, :] * dPsiDx[0, 0, :, :] + dPsiDy[0, 0, :, :] * dPsiDy[0, 0, :, :])
-        err2 = f_div_u[:, :] - psi[0, 0, :, :] * lap_psi[0, 0, :, :]
+        grad_f_dot_u, f_div_u = integrate_energy_exchange(
+            psi, dPsiDx, dPsiDy, dlon, dlat, cos_theta, sin_theta, cos_theta_inv
+        )
+
+        err1 = grad_f_dot_u - (dPsiDx * dPsiDx + dPsiDy * dPsiDy)
+        err2 = f_div_u - psi * lapPsi
         error_at_res_grad_f_dot_u[res] = integrate_horizontal(err1, dlon, dlat)
         error_at_res_f_div_u[res] = integrate_horizontal(err2, dlon, dlat)
         balance_error_at_res[res] = np.abs(error_at_res_grad_f_dot_u[res] + error_at_res_f_div_u[res]) / np.abs(
             error_at_res_grad_f_dot_u[res]
-        )
-
-        print(
-            str(res)
-            + "\t{:.4e}".format(error_at_res_grad_f_dot_u[res])
-            + "\t{:.4e}".format(error_at_res_f_div_u[res])
-            + "\t{:.4e}".format(balance_error_at_res[res])
         )
 
     convergence = np.zeros(num_resolutions - 1)
@@ -428,18 +424,17 @@ def test_budget(
     z = np.zeros((nt, nlev, nlat, nlon))
     sp = np.zeros((nt, nlat, nlon))
     zs = 1.0e5 * np.ones((nlat, nlon))
-    for ii in np.arange(nlev):
-        for jj in np.arange(nlat):
-            for kk in np.arange(nlon):
-                u[0, ii, jj, kk] = u_velocity_func(latitude[jj], longitude[kk], level[ii])
-                v[0, ii, jj, kk] = v_velocity_func(latitude[jj], longitude[kk], level[ii])
-                w[0, ii, jj, kk] = w_velocity_func(latitude[jj], longitude[kk], level[ii])
-                t[0, ii, jj, kk] = temperature_func(latitude[jj], longitude[kk], level[ii])
-                q[0, ii, jj, kk] = humidity_func(latitude[jj], longitude[kk], level[ii])
-                z[0, ii, jj, kk] = geopotential_func(latitude[jj], longitude[kk], level[ii])
-    for jj in np.arange(nlat):
-        for kk in np.arange(nlon):
-            sp[0, jj, kk] = surface_pressure_func(latitude[jj], longitude[kk])
+
+    lon2d, lat2d = np.meshgrid(longitude, latitude)
+    lev3d, lat3d, lon3d = np.meshgrid(level, latitude, longitude, indexing="ij")
+
+    u[0, :, :, :] = u_velocity_func(lat3d, lon3d, lev3d)
+    v[0, :, :, :] = v_velocity_func(lat3d, lon3d, lev3d)
+    w[0, :, :, :] = w_velocity_func(lat3d, lon3d, lev3d)
+    t[0, :, :, :] = temperature_func(lat3d, lon3d, lev3d)
+    q[0, :, :, :] = humidity_func(lat3d, lon3d, lev3d)
+    z[0, :, :, :] = geopotential_func(lat3d, lon3d, lev3d)
+    sp[0, :, :] = surface_pressure_func(lat2d, lon2d)
 
     field_names = ["u", "v", "w", "t", "q", "z", "sp", "zs"]
     ds = xr.Dataset(
@@ -494,7 +489,7 @@ def surface_geopotential(phi, theta):
             v_velocity,
             geopotential,
             surface_geopotential,
-            xr.DataArray([[-2.376601e17], [2.707613e17], [2.523455e17], [-2.854479e17]]).transpose(),
+            xr.DataArray([[-4.147951e15], [4.725676e15], [4.404260e15], [-4.982006e15]]).transpose(),
         ),
         (
             pd.date_range("2025-01-01", periods=1),
@@ -505,7 +500,7 @@ def surface_geopotential(phi, theta):
             v_velocity,
             geopotential,
             surface_geopotential,
-            xr.DataArray([[-2.376601e17], [2.707613e17], [2.523455e17], [-2.854479e17]]).transpose(),
+            xr.DataArray([[-4.147951e15], [4.725676e15], [4.404260e15], [-4.982006e15]]).transpose(),
         ),
     ],
 )
@@ -533,6 +528,10 @@ def test_exchanges(
     z = np.zeros((nt, nlev, nlat, nlon))
     sp = np.zeros((nt, nlat, nlon))
     zs = np.zeros((nlat, nlon))
+
+    lon2d, lat2d = np.meshgrid(longitude, latitude)
+    lev3d, lat3d, lon3d = np.meshgrid(level, latitude, longitude, indexing="ij")
+
     for ii in np.arange(nlev):
         for jj in np.arange(nlat):
             for kk in np.arange(nlon):
@@ -563,5 +562,5 @@ def test_exchanges(
         },
     )
 
-    E = energy_exchanges(ds, field_names)
+    E = energy_exchanges(ds, field_names, np.array([None]), np.array([None]))
     xr.testing.assert_allclose(E, expected, atol=1.0e-2)