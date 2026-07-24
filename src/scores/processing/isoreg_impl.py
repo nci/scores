c81607642329fa9fdf2c6e7a66f5a40f49f59a41
Maree Carroll <maree.carroll@gmail.com>
2026-07-24 18:08:20 +1000

    Fix lint issue

--- src/scores/processing/isoreg_impl.py
+++ src/scores/processing/isoreg_impl.py
@@ -493,7 +493,7 @@ def _contiguous_ir(
 
                 # regression value for current block
                 if weight is None:
-                    y_out[index] = solver(y[index:idx_next_block])  #type: ignore[ty:missing-argument]
+                    y_out[index] = solver(y[index:idx_next_block])  # type: ignore[ty:missing-argument]
                 else:
                     y_out[index] = solver(y[index:idx_next_block], weight[index:idx_next_block])  # type: ignore[ty:too-many-positional-arguments]
 
